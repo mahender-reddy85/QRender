7f25659b250298c692acecc7ed8bb478d0f0a2e3
Likki Mahender Reddy <likkimahenderreddy123@gmail.com>
2025-11-01 22:45:35 +0530

    b

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,15 +1,8 @@
-import { defineConfig } from "prisma/config";
+import { defineConfig } from "@prisma/config";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL || 'file:./dev.db',
-  },
+  schema: "./prisma/schema.prisma",
 });
